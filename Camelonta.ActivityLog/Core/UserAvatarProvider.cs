1c63e1fa8951cea0b522e69e3577cc321788ef65
MrSimon <simon.elmqvist@gmail.com>
2018-01-10 17:17:26 +0100

    avatar from umbraco user

--- Camelonta.ActivityLog/Core/UserAvatarProvider.cs
+++ Camelonta.ActivityLog/Core/UserAvatarProvider.cs
@@ -6,7 +6,10 @@ namespace Camelonta.ActivityLog.Core
     public class UserAvatarProvider
     {
         public static string GetAvatarUrl(IUser user)
-        { 
+        {
+            if (user.Avatar != null)
+                return string.Format("/media/{0}", user.Avatar);
+
             if (HttpContext.Current.Application["activitylog_user_" + user.Id] == null)
             {
                 var value = string.Format("http://www.gravatar.com/avatar/{0}&s=55&d=mm", GravatarHelper.HashEmailForGravatar(user.Email));
